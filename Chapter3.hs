03d1e06e56cd3bdbca62284c89b85e026c5e67dc
syunta <kitagawa@timedia.co.jp>
2017-10-03 20:44:43 +0900

    Answer 3.3

--- Chapter3.hs
+++ Chapter3.hs
@@ -22,3 +22,18 @@ insert' x E = T 1 x E E
 insert' x h@(T _ y a b)
   | x <= y    = makeT x E h
   | otherwise = makeT y a (insert' x b)
+
+-- 3.3
+fromList :: [Int] -> LeftistHeap
+fromList xs = fromList' $ map (\x -> insert x E) xs
+
+fromList' :: [LeftistHeap] -> LeftistHeap
+fromList' [x] = x
+fromList' xs = fromList' $ map2 merge E xs
+
+map2 :: (a -> a -> b) -> a -> [a] -> [b]
+map2 _ _ []       = []
+map2 f a [x]      = [f a x]
+map2 f a (x:y:xs) = f x y : map2 f a xs
+
+-- log2(n) * log2(n) は常に log2(n) * log2(n) <= n を満たすので、fromList は O(n) 時間しかかからない
--- Chapter3Spec.hs
+++ Chapter3Spec.hs
@@ -11,3 +11,6 @@ main = hspec $ do
     it "inserts element to the leftist heap" $ do
       (insert 5 E -: insert 8 -: insert 4 -: insert 1 -: insert 10) `shouldBe`
         (insert' 5 E -: insert' 8 -: insert' 4 -: insert' 1 -: insert' 10)
+  describe "fromList" $ do
+    it "generates a LeftistHeap from array" $ do
+      merge (insert 7 E -: insert 2) (insert 1 E -: insert 4) `shouldBe` fromList [7,2,1,4]
